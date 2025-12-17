10a6b5591432688e8569bda309b15829cedf07dc
Vincent de Paul NTIHINYURWA <94695890+Ntihinyurwa@users.noreply.github.com>
2025-12-17 11:20:02 -0600

    Add query for doctors with high average invoices
    Added a query to find doctors with above-average invoices compared to the clinic's average.

--- data-modeling-sql/database-design-advanced-sql/queries/advanced_queries.sql
+++ data-modeling-sql/database-design-advanced-sql/queries/advanced_queries.sql
@@ -1,1 +1,10 @@
+-- Doctors whose average invoice exceeds clinic average
+SELECT d.first_name, d.last_name
+FROM Doctor d
+JOIN Appointment ap ON d.doctor_id = ap.doctor_id
+JOIN Visit v ON ap.appointment_id = v.appointment_id
+JOIN Invoice i ON v.visit_id = i.visit_id
+GROUP BY d.first_name, d.last_name
+HAVING AVG(i.total_amount) >
+       (SELECT AVG(total_amount) FROM Invoice);
 
